cce28d4c04e655f6dcffded5393b56c92879581a
Aditya Sharma <3008.aditya.sharma@gmail.com>
2025-01-24 22:53:29 +0530

    feat: implement PATCH route for generating invite codes

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -65,7 +65,7 @@ export const InviteModal = () => {
 						<Input
 							disabled={isLoading}
 							className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-							value={inviteUrl}
+							defaultValue={inviteUrl}
 						></Input>
 						<Button disabled={isLoading} onClick={onCopy} size="icon">
 							{copied ? (

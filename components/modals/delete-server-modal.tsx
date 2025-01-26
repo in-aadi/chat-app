38a6185f94811e8f638ceb1c1b631d943fa4b03e
Aditya Sharma <3008.aditya.sharma@gmail.com>
2025-01-26 23:12:57 +0530

    feat: add Delete Server modal and implement DELETE route for server deletion

--- components/modals/delete-server-modal.tsx
+++ components/modals/delete-server-modal.tsx
@@ -14,11 +14,11 @@ import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
-export const LeaveServerModal = () => {
+export const DeleteServerModal = () => {
 	const { isOpen, onClose, type, data } = useModal();
 	const router = useRouter();
 
-	const isModelOpen = isOpen && type === "leaveServer";
+	const isModelOpen = isOpen && type === "deleteServer";
 	const { server } = data;
 
 	const [isLoading, setIsLoading] = useState(false);
@@ -26,7 +26,7 @@ export const LeaveServerModal = () => {
 	const onClick = async () => {
 		try {
 			setIsLoading(true);
-			await axios.patch(`/api/servers/${server?.id}/leave`);
+			await axios.delete(`/api/servers/${server?.id}`);
 			onClose();
 			router.refresh();
 			router.push("/");
@@ -42,13 +42,14 @@ export const LeaveServerModal = () => {
 			<DialogContent className="bg-white text-black p-0 overflow-hidden">
 				<DialogHeader className="pt-8 px-6">
 					<DialogTitle className="text-2xl text-center font-bold">
-						Leave Server
+						Delete Server
 					</DialogTitle>
 					<DialogDescription className="text-center text-zinc-500">
-						Are you sure you want to leave {" "}
+						Are you sure you want to do this? <br />
 						<span className="font-semibold text-indigo-500">
 							{server?.name}
-						</span>
+						</span>{" "}
+						will be permanently deleted
 					</DialogDescription>
 				</DialogHeader>
 				<DialogFooter className="bg-gray-100 px-6 py-4">

3be5566d71b723d4ba65bfb43546ded88261d116
Aditya Sharma <3008.aditya.sharma@gmail.com>
2025-01-28 15:52:26 +0530

    feat: implement ChannelIdPage with authentication and channel/member validation, add ChatHeader and MobileToggle components

--- app/(main)/(routes)/server/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/server/[serverId]/channels/[channelId]/page.tsx
@@ -1,6 +1,45 @@
-const ChannelIdPage = () => {
-    return ( <div>Channel ID Page</div>
-     );
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+	params: {
+		serverId: string;
+		channelId: string;
+	};
 }
- 
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+	const profile = await currentProfile();
+
+	if (!profile) {
+		return RedirectToSignIn({ redirectUrl: "/sign-in" });
+	}
+
+	const channel = await db.channel.findUnique({
+		where: {
+			id: params.channelId,
+		},
+	});
+
+	const member = await db.member.findFirst({
+		where: {
+			serverId: params.serverId,
+			profileId: profile.id,
+		},
+	});
+
+	if (!channel || !member) {
+		return redirect("/");
+	}
+
+	return (
+		<div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+			<ChatHeader serverId={channel.serverId} name={channel.name} type="channel" />
+		</div>
+	);
+};
+
 export default ChannelIdPage;
